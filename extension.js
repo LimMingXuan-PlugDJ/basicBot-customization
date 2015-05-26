d745180f12702723309153e4b78eb3c0ccf21707
LimMingXuan-PlugDJ <mingxuan_lim@hotmail.com>
2015-05-26 15:45:20 +0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -55,11 +55,11 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-        botName: "basicBot",
+        botName: "WP-Bot",
         language: "english",
-        startupCap: 1, // 1-200
+        startupCap: 200, // 1-200
         startupVolume: 0, // 0-100
-        startupEmoji: false, // true or false
+        startupEmoji: true, // true or false
         cmdDeletion: true,
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         maximumAfk: 120,
